935d1faa943fc330d0d907c72647fbf29aa4b9b8
KAWAKUBO Toru <quantum@kwkbtr.info>
2019-05-05 20:25:48 +0900

    Try to disable version display

--- conf.py
+++ conf.py
@@ -55,6 +55,9 @@ exclude_patterns = ['_build', '**.ipynb_checkpoints', 'Thumbs.db', '.DS_Store']
 #
 html_theme = 'sphinx_qndojo_theme'
 html_theme_path = ['.']
+html_theme_options = {
+    'display_version': False
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
