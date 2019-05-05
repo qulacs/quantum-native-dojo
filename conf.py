334e7f44591b6295a26dd34792a082658a13b787
KAWAKUBO Toru <quantum@kwkbtr.info>
2019-05-05 13:56:43 +0900

    Font settings: body font and math font

--- conf.py
+++ conf.py
@@ -60,3 +60,9 @@ html_theme_path = ['.']
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+mathjax_path = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/latest.js?config=TeX-MML-AM_CHTML'
+mathjax_config = {
+    'CommonHTML': {
+    }
+}
